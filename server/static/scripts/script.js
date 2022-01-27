2133bcf28ccaaf762d2a025b2a3a89dac17440e0
Vincent Ngo <nbkntu@gmail.com>
2022-01-26 23:38:18 -0800

    refactor: rendering bounding box

--- server/static/scripts/script.js
+++ server/static/scripts/script.js
@@ -1,3 +1,271 @@
+class AppController {
+  constructor(canvasEl, imageEl) {
+    this.appState = new AppState();
+
+    this.canvasEl = canvasEl;
+    this.setupCanvasEvents();
+
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.resetOffset();
+
+    this.imageEl = imageEl;
+
+    this.canvasUtil = new CanvasUtil();
+  }
+
+  // reset offset coordinates of canvas element
+  resetOffset() {
+    var box = canvas.getBoundingClientRect();
+    this.offsetX = box.left;
+    this.appStateoffsetY = box.top;
+  }
+
+  setupCanvasEvents() {
+    this.canvasEl.onmousedown = this.handleMouseDown;
+    this.canvasEl.onmousemove = this.handleMouseMove;
+    this.canvasEl.onmouseup = this.handleMouseUp;
+    this.canvasEl.onmouseout = this.handleMouseOut;
+    this.canvasEl.ondblclick = this.handleDoubleClick;
+  }
+
+  handleMouseDown(e) {
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+
+    // record mouse start position
+    var startX = parseInt(e.clientX - this.offsetX);
+    var startY = parseInt(e.clientY - this.offsetY);
+
+    this.appState.rectPointIndex = this.canvasUtil.getPointClicked(this.appState.rect, startX, startY);
+    if (this.appState.rectPointIndex) {
+      this.appState.isDown = true;
+      console.log(this.appState.isDown);
+    }
+  }
+
+  handleMouseMove(e) {
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  handleMouseUp(e) {
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  handleMouseOut(e) {
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  handleDoubleClick(e) {
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  draw() {
+    const ctx = this.getCanvasContext();
+
+    // clear canvas
+    this.canvasUtil.clearRect(ctx, 0, 0, canvas.width, canvas.height);
+
+    // reset canvas size
+    canvas.width = this.imageEl.naturalWidth;
+    canvas.height = this.imageEl.naturalHeight;
+
+    // draw image html element onto canvas, keep image original size
+    this.canvasUtil.drawImage(ctx, this.imageEl, 0, 0);
+
+    if (this.appState.annotationMode == 'bounding-box') {
+      this.canvasUtil.drawBoundingBox(ctx, this.appState.rect, this.appState.dotSize);
+    } else if (this.appState.annotationMode == 'contour') {
+      this.canvasUtil.drawContour(ctx, this.appState.poly);
+    }
+  }
+
+  getCanvasContext() {
+    const canvas = this.canvasEl;
+    return canvas.getContext('2d');
+  }
+
+  getBoundingBox(ctx, filename) {
+    console.log(filename);
+    var that = this;
+    getBoundingBoxes(filename).then(
+      function(resp) {
+          that.getBoundingBoxCallback(resp);
+      },
+      function(error) {
+          console.log("Error: ", error);
+      });
+  }
+
+  getBoundingBoxCallback(resp) {
+    console.log(resp);
+
+    var rect = this.appState.rect;
+
+    rect.x1 = resp.bounding_box[0][0];
+    rect.y1 = resp.bounding_box[0][1];
+    rect.x2 = resp.bounding_box[0][2];
+    rect.y2 = resp.bounding_box[0][3];
+
+    console.log(rect);
+
+    this.appState.annotationMode = 'bounding-box';
+
+    this.draw();
+  }
+};
+
+class CanvasUtil {
+  constructor() {}
+
+  clearRect(ctx, x, y, width, height) {
+    ctx.clearRect(x, y, width, height);
+  }
+
+  drawImage(ctx, img, x, y) {
+    ctx.drawImage(img, 0, 0);
+  }
+
+  drawPointRect(ctx, x, y, dotSize) {
+    ctx.rect(
+        x - dotSize / 2,
+        y - dotSize / 2,
+        dotSize,
+        dotSize
+    );
+    ctx.stroke();
+  }
+
+  drawBoundingBox(ctx, rect, dotSize) {
+    // draw bounding box rectangle
+    ctx.beginPath();
+    ctx.rect(rect.x1, rect.y1, rect.x2 - rect.x1, rect.y2 - rect.y1);
+    ctx.strokeStyle = 'yellow';
+    ctx.stroke();
+
+    // draw corner boxes
+    this.drawPointRect(ctx, rect.x1, rect.y1, dotSize);
+    this.drawPointRect(ctx, rect.x1, rect.y2, dotSize);
+    this.drawPointRect(ctx, rect.x2, rect.y1, dotSize);
+    this.drawPointRect(ctx, rect.x2, rect.y2, dotSize);
+  }
+
+  drawContour(ctx, poly, dotSize) {
+    ctx.beginPath();
+
+    // draw polygon
+    ctx.beginPath();  // need this for clearRect to work
+
+    ctx.moveTo(poly.points[0].x,poly.points[0].y);
+    for (var i=0; i < poly.points.length; i++) {
+      ctx.lineTo(poly.points[i].x,poly.points[i].y);
+    }
+    ctx.lineTo(poly.points[0].x, poly.points[0].y);
+
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth=1;
+
+    ctx.stroke();
+
+    // draw corner boxes
+    for(var i=0; i < poly.points.length; i++) {
+      // draw rectangle at the points
+      drawPointRect(ctx, poly.points[i].x, poly.points[i].y, dotSize);
+    }
+  }
+
+  inClickRange(x0, y0, dotSize, x, y) {
+    const x1 = x0 - dotSize/2;
+    const x2 = x0 + dotSize/2;
+    const y1 = y0 - dotSize/2;
+    const y2 = y0 + dotSize/2;
+    if (x1 <= x && x2 >= x && y1 <= y && y2 >= y) {
+      return true;
+    }
+    return false;
+  };
+
+  getPointClicked(rect, x, y) {
+    // 1 2
+    // 3 4
+    if (inClickRange(rect.x1, rect.y1, dotSize, x, y)) {
+      return 1;
+    }
+    if (inClickRange(rect.x2, rect.y1, dotSize, x, y)) {
+      return 2;
+    }
+    if (inClickRange(rect.x1, rect.y2, dotSize, x, y)) {
+      return 3;
+    }
+    if (inClickRange(rect.x2, rect.y2, dotSize, x, y)) {
+      return 4;
+    }
+    return null;
+  };
+};
+
+class AppState {
+  constructor() {
+    this.isDown = false;
+
+    // currently seelected point index of bounding box rectangle
+    this.rectPointIndex = null;
+
+    // current mode: 'ready' / 'bounding-box' / 'polygon'
+    this.annotationMode = 'ready';
+
+    // size of control rectangle
+    this.dotSize = 8;
+
+    // bounding box rectangle
+    this.rect = {
+      x1: 20,
+      y1: 20,
+      x2: 300,
+      y2: 200
+    };
+
+    // polygon contour
+    this.poly = {
+      points:[ {x:175, y:75}, {x:250, y:150}, {x:175, y:225}, {x:100, y:180} ],
+    }
+  }
+
+  updateRectCoordinates(dx, dy) {
+    if (!this.rectPointIndex) {
+      return;
+    }
+    switch(this.rectPointIndex) {
+      case 1:
+        rect.x1 += dx;
+        rect.y1 += dy;
+        break;
+      case 2:
+        rect.x2 += dx;
+        rect.y1 += dy;
+        break;
+      case 3:
+        rect.x1 += dx;
+        rect.y2 += dy;
+        break;
+      case 4:
+        rect.x2 += dx;
+        rect.y2 += dy;
+        break;
+    }
+  };
+};
+
+
 const getBoundingBoxes = async (filename) => {
     const response = await fetch('/get_bounding_boxes', {
       method: 'POST',
